37f430bb35f1c275a9aba0d06d3d327c8e134d5e
st64138 <st64138@upce.cz>
2025-02-10 15:01:18 +0100

    CV-01: Init commit

---
+++ backend/src/main/java/com/example/demo/controllers/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping
+    public String greeting() {
+        return greetingService.sayGreeting();
+    }
+}
