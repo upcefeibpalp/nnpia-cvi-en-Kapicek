826614573b5f45587389375e8be91ad3cb50cddc
st64138 <st64138@upce.cz>
2025-02-10 15:26:39 +0100

    CV-01: rem autowired

--- backend/src/main/java/com/example/demo/controllers/GreetingController.java
+++ backend/src/main/java/com/example/demo/controllers/GreetingController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controllers;
 
 import com.example.demo.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ public class GreetingController {
 
     private final GreetingService greetingService;
 
-    @Autowired
     public GreetingController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
